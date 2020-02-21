010ed9f874e1c1cdf8432e272a991b5f4734500d
Quang Nguyen <quangnguyen@Quangs-MacBook-Pro.local>
2020-02-21 12:19:41 -0800

    fixed a small amount of bugs

--- lost_n_found_app/static/script.js
+++ lost_n_found_app/static/script.js
@@ -1,3 +1,33 @@
-$(document).ready(function () {
+// JQUERY, JS
 
-});
+// function showAll() {
+//     window.location.href = "/home/all";
+// };
+
+// function showMine() {
+//     window.location.href = "/home/mine";
+// };
+
+// $(document).ready(function () {
+//     $('.found-section').hide();
+
+//     $('.type').on('click', function (event) {
+//         id = $(event.target).attr('id');
+
+//         if ($(`#${id}`).hasClass('active')) {
+//             return;
+//         }
+
+//         if (id === "found-btn") {
+//             $(`#${id}`).addClass('active');
+//             $('#lost-btn').removeClass('active');
+//             $('.found-section').show();
+//             $('.lost-section').hide();
+//         } else {
+//             $(`#${id}`).addClass('active');
+//             $('#found-btn').removeClass('active');
+//             $('.found-section').hide();
+//             $('.lost-section').show();
+//         }
+//     });
+// });

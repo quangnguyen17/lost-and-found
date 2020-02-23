e704b9f799da670004665886596d5ef3e724c0d9
Quang Nguyen <quangnguyen@Quangs-MacBook-Pro.local>
2020-02-23 13:29:11 -0800

    added files and some modifications

--- lost_n_found_app/urls.py
+++ lost_n_found_app/urls.py
@@ -2,14 +2,16 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # /
+    # / {{ ROOT DIRECTORY }}
     path('', views.index),
-    # /home/
+    # /home
     path('home', views.home),
     path('home/add', views.add),
     path('home/logout', views.log_out),
+    path('home/profile/<int:user_id>', views.profile),
     # /actions
     path('add_item', views.add_item),
+    path('update_campus', views.update_campus),
     path('found_item/<int:item_id>', views.found_item),
     path('unfound_item/<int:item_id>', views.unfound_item),
     path('remove_item/<int:item_id>', views.remove_item),
